a14fe64531fd8ff63a3720b77718c017c7eb1b56
hnogi <hnogi@student.42tokyo.jp>
2026-09-18 15:10:55 +0900

    f

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -38,3 +38,47 @@ int main()
 
     return 0;
 }
+
+// #include <iostream>
+
+// int main()
+// {
+//     IMateriaSource* ice_src = new MateriaSource();
+//     IMateriaSource* cure_src = new MateriaSource();
+
+//     AMateria* ice = new Ice();
+//     AMateria* cure = new Cure();
+
+//     for(int i = 0; i < 4; i++)
+//         ice_src->learnMateria(ice);
+
+//     ICharacter* me = new Character("me");
+//     ICharacter* bob = new Character("bob");
+//     AMateria* tmp;
+
+//     for(int i = 0; i < 4; i++)
+//     {
+//         tmp = ice_src->createMateria("ice");
+//         me->equip(tmp);
+//         me->use(i, *bob);
+//     }
+//     std::cout << "------------" << std::endl;
+//     me->unequip(2);
+//     cure_src->learnMateria(cure);
+
+//     tmp = cure_src->createMateria("cure");
+//     me->equip(tmp);
+//     for(int i = 0; i < 4; i++)
+//     {
+//         me->use(i, *bob);
+//     }
+
+//     delete bob;
+//     delete me;
+//     delete ice_src;
+//     delete ice;
+//     delete cure;
+//     delete cure_src;
+
+//     return 0;
+// }
